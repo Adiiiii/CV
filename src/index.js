c3a0fce5a53c030ef69cd4c9591fbadebc8e4b60
Adi <saxenaadi92@gmail.com>
2021-06-28 21:12:17 +0530

    simplified imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-(async () => {
-  console.log("Trying to optimize parcel asset loading");
-  await require("../node_modules/@fortawesome/fontawesome-free/css/all.css");
-  console.log(new Date().getSeconds());
-  await require("../node_modules/@fortawesome/fontawesome-free/js/all.js");
-  console.log(new Date().getSeconds());
-  await require("./styles.scss");
-  console.log(new Date().getSeconds());
-})();
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+import "../node_modules/@fortawesome/fontawesome-free/js/all.js";
+import "./styles.scss";
